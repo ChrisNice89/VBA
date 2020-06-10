21104354ac78ed428416e6732ec399c6c4f81047
ChrisNice89 <50628492+ChrisNice89@users.noreply.github.com>
2020-06-11 01:18:08 +0200

    Update

---
+++ CSharp/SqlTools.Tests/ConditionGroupTests.cs
@@ -0,0 +1,71 @@
+﻿using AccessCodeLib.Data.SqlTools.Sql;
+using NUnit.Framework;
+using AccessCodeLib.Data.Common.Sql;
+
+namespace AccessCodeLib.Data.SqlTools.Tests
+{
+    [TestFixture]
+    class ConditionGroupTests
+    {
+        private ConditionGroup _conditionGroup;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _conditionGroup = new ConditionGroup();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _conditionGroup = null;
+        }
+
+        [Test]
+        public void Add_1FieldCondition_CheckValue()
+        {
+            var expected = new FieldCondition(new Field("F1"), RelationalOperators.Equal, 123);
+
+            _conditionGroup.Add(new Field("F1"), RelationalOperators.Equal, 123);
+
+            var actual = (IFieldCondition)_conditionGroup.Conditions[0];
+            Assert.AreEqual(expected.Value, actual.Value);
+        }
+
+        [Test]
+        public void Add_2FieldConditions_CheckValueItem2()
+        {
+            var expected = new FieldCondition(new Field("F2"), RelationalOperators.GreaterThan, 0);
+
+            _conditionGroup.Add(new Field("F1"), RelationalOperators.Equal, 123)
+                           .Add(new Field("F2"), RelationalOperators.GreaterThan, 0);
+
+            var actual = (IFieldCondition)(_conditionGroup.Conditions[1]);
+            Assert.AreEqual(expected.Operator, actual.Operator);
+        }
+
+        [Test]
+        public void Add_1ConditionGroup_CheckValue()
+        {
+            var expected = new FieldCondition(new Field("F1"), RelationalOperators.Equal, 123);
+            var groupToAdd = new ConditionGroup(new [] {expected});
+
+            _conditionGroup.Add(groupToAdd);
+
+            var actual = (IFieldCondition)((IConditionGroup)_conditionGroup.Conditions[0]).Conditions[0];
+            Assert.AreEqual(expected.Value, actual.Value);
+        }
+
+        [Test]
+        public void Add_2FieldsInConditionGroup_CheckValue()
+        {
+            var expected = new FieldCondition(new Field("F1"), RelationalOperators.Equal, new Field("F2"));
+
+            _conditionGroup.Add(new Field("F1"), RelationalOperators.Equal, new Field("F2"));
+
+            var actual = ((IFieldCondition)_conditionGroup.Conditions[0]);
+            Assert.AreEqual(expected.Value.GetType(), actual.Value.GetType());
+
+        }
+    }
+}
---
+++ CSharp/SqlTools.Tests/SelectStatementTests.cs
@@ -0,0 +1,23 @@
+﻿using AccessCodeLib.Data.SqlTools.Sql;
+using NUnit.Framework;
+
+namespace AccessCodeLib.Data.SqlTools.Tests
+{
+    [TestFixture] 
+    public class SelectStatementTests
+    {
+        [Test]
+        public void SelectStatement_Fields_AddFieldsByNames()
+        {
+            var s = new SelectStatement();
+            s.Fields.Add("F1", "F2", "F3");
+
+            // TODO: Was bringt dieser Test?
+            // ... Das was oben steht: er prüft, ob Add(...) aus der Fields-Auflistung funktioniert.
+            // Soll man so einen Test später entfernen, wenn die Programmierung so einer Klasse abgeschlossen ist? 
+            // ... ich bin der Meinung, dass man das nicht machen soll.
+            var f = (FieldsStatement) s;
+            Assert.AreEqual(s.Fields.Count, f.Fields.Count);
+        }
+    }
+}
